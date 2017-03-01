f4d51bf2048bf3e64a67db74b6b614c68c24009a
celine-fucci <Celine@Celines-MacBook-Pro.local>
2017-03-01 11:04:13 -0500

    Setting up Branch

--- main.cpp
+++ main.cpp
@@ -9,39 +9,40 @@
 
 int main(int argc, char** argv)
 {
-    SDL_Window *window = NULL;          
+  //Test Comment
+    SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_Texture *texture = NULL;
-    
+
     //for random number generation
     srand(time(NULL));
-    
+
     SDL_Init(SDL_INIT_EVERYTHING);
-    
+
     //create window 1080x720
     window = SDL_CreateWindow("Map", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1080, 720, SDL_WINDOW_SHOWN);
     if(window == NULL)
         std::cout << "window" << SDL_GetError() << std::endl;
-    
+
     //create renderer
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
     if(renderer == NULL)
         std::cout << "renderer" << SDL_GetError() << std::endl;
-    
+
     //create an array of ropes
     Rope ropes[total];
-    
+
     //creates points but they cannot be 200 pixels near each other
     //also makes it so that ropes cannot be over 300 pixels away from each other
-    
+
     //angle of the rope
     int angle = 0;
-    
+
     int xPos = 0;
     int yPos = rand() % 50 + 50;
-    
+
     int situation = 0;
-    
+
     for(int i = 0; i < total; i++)
     {
         xPos += rand() % 50 + 100;
@@ -50,27 +51,27 @@ int main(int argc, char** argv)
             xPos = rand() % 50 + 100;
             yPos += rand() % 100 + 100;
         }
-        
+
         ropes[i].setPointX(xPos);
         ropes[i].setPointY(yPos);
-        
-        
+
+
         angle = rand() % 180;
         ropes[i].setAngle(angle);
         ropes[i].setTexture("/images/Rope.png", renderer);
-        
+
     }
-    
-    
+
+
     //running game
     bool running = true;
     SDL_Event event;
-    
-    //player rect for testing. 
+
+    //player rect for testing.
     SDL_Renderer* rectRender = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
     SDL_Rect player = {0, 0, 30, 30};
-    
-    
+
+
     while(running)
     {
         SDL_PollEvent(&event);
@@ -80,7 +81,7 @@ int main(int argc, char** argv)
             case SDL_QUIT:
                 running = false;
                 break;
-            
+
             //move the player
             //unsure how to do diagonal
             case SDL_KEYDOWN:
@@ -100,38 +101,38 @@ int main(int argc, char** argv)
                         break;
                 }
                 break;
-                
+
             default:
                 break;
-        
+
         }
         //makes window background not black
         SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
-        
+
         //resets the renderer
         SDL_RenderClear(renderer);
-        
+
         //draws a rectangle
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         //renders ropes to screen
         for(int i = 0; i < total; i++)
             ropes[i].render(ropes[i].getPoint()->x, ropes[i].getPoint()->y, NULL, renderer, ropes[i].getAngle(), NULL, SDL_FLIP_NONE);
-        
+
         SDL_RenderFillRect(renderer, &player);
-        
+
 
         //outputs the renderer
         SDL_RenderPresent(renderer);
-        
+
         //restricts to 60fps
         SDL_Delay(1000/60);
     }
-    
+
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyTexture(texture);
-    
+
     SDL_Quit();
-    
+
     return 0;
 }
