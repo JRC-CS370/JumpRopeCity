e597c236d585979a8f6ad9d7662ddd9a28db26f3
celine-fucci <Celine@Celines-MBP.home>
2017-03-10 20:14:48 -0500

    Implementation File
    Set up the Player.cpp file

---
+++ Player.cpp
@@ -0,0 +1,52 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+#include <iostream>
+#include "Player.h"
+
+using namespace std;
+
+Player::Player()
+{
+
+}
+
+void Player::setCompass()
+{
+
+}
+
+void Player::setDirection()
+{
+
+}
+
+void Player::setThrottle()
+{
+
+}
+
+void Player::setSpeed()
+{
+
+}
+
+int Player::getCompass()
+{
+
+}
+
+int Player::getDirection()
+{
+
+}
+
+int Player::getThrottle()
+{
+
+}
+
+int Player::getSpeed()
+{
+
+}
