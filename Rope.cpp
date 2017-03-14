bf84427f8bfff03eac9e6f575444a3622ca97bef
jay pestillo <jaypestillo@gmail.com>
2017-03-13 23:08:47 -0400

    I didn't change any specific code, but just auto-indented all of the current code and commented some of the brackets because I couldn't trace where certain things were in the main.cpp

--- Rope.cpp
+++ Rope.cpp
@@ -3,142 +3,142 @@
 
 Rope::Rope()
 {
-    //Initialize
-    mTexture = NULL;
-    mWidth = 100;
-    mHeight = 15;
-    
-    midpoint.x = 0;
-    midpoint.y = 0;
-    
-    pointA.x = 0;
-    pointA.y = 0;
-    
-    pointB.x = 0;
-    pointB.y = 0;
-    
-    angle = 0;
+  //Initialize
+  mTexture = NULL;
+  mWidth = 100;
+  mHeight = 15;
+
+  midpoint.x = 0;
+  midpoint.y = 0;
+
+  pointA.x = 0;
+  pointA.y = 0;
+
+  pointB.x = 0;
+  pointB.y = 0;
+
+  angle = 0;
 }
 
 Rope::~Rope()
 {
-    free();
+  free();
 }
 
 void Rope::setTexture(std::string path, SDL_Renderer* renderer)
 {
-    if(mTexture != NULL)
-        SDL_DestroyTexture(mTexture);
+  if(mTexture != NULL)
+  SDL_DestroyTexture(mTexture);
 
-    //Load image
-    SDL_Surface* loadedSurface = IMG_Load((JR_HOME + path).c_str());
-    if(loadedSurface == NULL)
-        std::cout << "Load image error " << IMG_GetError() << std::endl;
-    else
-    {
-        //Create texture from surface pixels
-        mTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-        if(mTexture == NULL)
-            std::cout << "Could not create texture from surface " << SDL_GetError() << std::endl;
+  //Load image
+  SDL_Surface* loadedSurface = IMG_Load((JR_HOME + path).c_str());
+  if(loadedSurface == NULL)
+  std::cout << "Load image error " << IMG_GetError() << std::endl;
+  else
+  {
+    //Create texture from surface pixels
+    mTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    if(mTexture == NULL)
+    std::cout << "Could not create texture from surface " << SDL_GetError() << std::endl;
 
-        //Get rid of old loaded surface
-        SDL_FreeSurface(loadedSurface);
-    }
+    //Get rid of old loaded surface
+    SDL_FreeSurface(loadedSurface);
+  }
 }
 
 void Rope::free()
 {
-    //Free texture if it exists
-    if( mTexture != NULL )
-    {
-        SDL_DestroyTexture(mTexture);
-        mTexture = NULL;
-        mWidth = 0;
-        mHeight = 0;
-    }
-    
+  //Free texture if it exists
+  if( mTexture != NULL )
+  {
+    SDL_DestroyTexture(mTexture);
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+  }
+
 }
 
 void Rope::render(int x, int y, SDL_Rect* rect, SDL_Renderer* renderer, double angle, SDL_Point* center, SDL_RendererFlip flip)
 {
-    //Set rendering space and render to screen
-    SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+  //Set rendering space and render to screen
+  SDL_Rect renderQuad = {x, y, mWidth, mHeight};
 
-    //Set rect rendering dimensions
-    if(rect != NULL)
-    {
-        renderQuad.w = rect->w;
-        renderQuad.h = rect->h;
-    }
+  //Set rect rendering dimensions
+  if(rect != NULL)
+  {
+    renderQuad.w = rect->w;
+    renderQuad.h = rect->h;
+  }
 
-    //Render to screen
-    SDL_RenderCopyEx(renderer, mTexture, rect, &renderQuad, angle, center, flip);
+  //Render to screen
+  SDL_RenderCopyEx(renderer, mTexture, rect, &renderQuad, angle, center, flip);
 }
 
 int Rope::getWidth()
 {
-    return mWidth;
+  return mWidth;
 }
 
 int Rope::getHeight()
 {
-    return mHeight;
+  return mHeight;
 }
 
 SDL_Point* Rope::getMidPoint()
 {
-    return &midpoint;
+  return &midpoint;
 }
 
 SDL_Point* Rope::getPointA()
 {
-    return &pointA;
+  return &pointA;
 }
 SDL_Point* Rope::getPointB()
 {
-    return &pointB;
+  return &pointB;
 }
 
 int Rope::getAngle()
 {
-    return angle;
+  return angle;
 }
 
 SDL_Texture* Rope::getTexture()
 {
-    return mTexture;
+  return mTexture;
 }
 
 void Rope::setMidPointX(int newX)
 {
-    midpoint.x = newX;
+  midpoint.x = newX;
 }
 
 void Rope::setMidPointY(int newY)
 {
-    midpoint.y = newY;
+  midpoint.y = newY;
 }
 
 void Rope::setAngle(int newAngle)
 {
-    if(newAngle == 0)
-	angle = 0;
-    else
-   	angle = 90;
+  if(newAngle == 0)
+  angle = 0;
+  else
+  angle = 90;
 
-    setPointA(angle);
-    setPointB(angle);
-    
+  setPointA(angle);
+  setPointB(angle);
+
 }
 
 void Rope::setPointA(int angle)
 {
-    pointA.x = midpoint.x + (cos(angle * (PI/180)) * mWidth/2);
-    pointA.y = midpoint.y + (sin(angle * (PI/180)) * mWidth/2);
+  pointA.x = midpoint.x + (cos(angle * (PI/180)) * mWidth/2);
+  pointA.y = midpoint.y + (sin(angle * (PI/180)) * mWidth/2);
 }
 
 void Rope::setPointB(int angle)
 {
-    pointB.x = midpoint.x - (cos(angle * (PI/180)) * mWidth/2);
-    pointB.y = midpoint.y - (sin(angle * (PI/180)) * mWidth/2);
+  pointB.x = midpoint.x - (cos(angle * (PI/180)) * mWidth/2);
+  pointB.y = midpoint.y - (sin(angle * (PI/180)) * mWidth/2);
 }
--- main.cpp
+++ main.cpp
@@ -12,131 +12,137 @@
 int main(int argc, char** argv)
 {
 
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
-    SDL_Texture *texture = NULL;
+  SDL_Window *window = NULL;
+  SDL_Renderer *renderer = NULL;
+  SDL_Texture *texture = NULL;
 
-    //for random number generation
-    srand(time(NULL));
+  //for random number generation
+  srand(time(NULL));
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Init(SDL_INIT_EVERYTHING);
 
-    //create window 1080x720
-    window = SDL_CreateWindow("Map", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 860, 720, SDL_WINDOW_SHOWN);
-    if(window == NULL)
-        std::cout << "window" << SDL_GetError() << std::endl;
+  //create window 1080x720
+  window = SDL_CreateWindow("Map", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 860, 720, SDL_WINDOW_SHOWN);
+  if(window == NULL)
+  {
+    std::cout << "window" << SDL_GetError() << std::endl;
+  }//end of if statement
 
-    //create renderer
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
-    if(renderer == NULL)
-        std::cout << "renderer" << SDL_GetError() << std::endl;
+  //create renderer
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
+  if(renderer == NULL)
+  {
+    std::cout << "renderer" << SDL_GetError() << std::endl;
+  }//end of if statement
 
-    //create an array of ropes
-    Rope ropes[5][5];
+  //create an array of ropes
+  Rope ropes[5][5];
 
-    //angle of the rope
-    int angle = 0;
-    int xPos = 0;
-    int yPos = 100;
-    int makeRope = 0;
+  //angle of the rope
+  int angle = 0;
+  int xPos = 0;
+  int yPos = 100;
+  int makeRope = 0;
 
+  for(int y = 0; y < total; y++)
+  {
+    for(int x = 0; x < total; x++)
+    {
+      makeRope = rand() % 3;
+
+      if(makeRope == 0)
+      {
+        xPos+=125;
+
+        ropes[y][x].setMidPointX(xPos);
+        ropes[y][x].setMidPointY(yPos);
+        angle = rand() % 2;
+        ropes[y][x].setAngle(angle);
+        ropes[y][x].setTexture("/images/Rope.png", renderer);
+      }//end of if statement
+    }//end of for loop
+    yPos += 125;
+    xPos = 0;
+  }//end of for loop
+
+
+  //running game
+  bool running = true;
+  SDL_Event event;
+
+  //player rect for testing.
+  SDL_Renderer* rectRender = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
+  SDL_Rect player = {0, 0, 30, 30};
+
+
+  while(running)
+  {
+    SDL_PollEvent(&event);
+    switch (event.type)
+    {
+      //close window
+      case SDL_QUIT:
+      running = false;
+      break;
+
+      //move the player
+      //unsure how to do diagonal
+      case SDL_KEYDOWN:
+      switch(event.key.keysym.sym)
+      {
+        case SDLK_LEFT:
+        player.x--;
+        break;
+        case SDLK_RIGHT:
+        player.x++;
+        break;
+        case SDLK_UP:
+        player.y--;
+        break;
+        case SDLK_DOWN:
+        player.y++;
+        break;
+      }
+      break;
+
+      default:
+      break;
+
+    }
+    //makes window background not black
+    SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
+
+    //resets the renderer
+    SDL_RenderClear(renderer);
+
+    //draws a rectangle
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    //renders ropes to screen
     for(int y = 0; y < total; y++)
     {
-	for(int x = 0; x < total; x++)
-    	{
-    	    makeRope = rand() % 3;
+      for(int x = 0; x < total; x++)
+      {
+        ropes[y][x].render(ropes[y][x].getMidPoint()->x, ropes[y][x].getMidPoint()->y, NULL, renderer, ropes[y][x].getAngle(), NULL, SDL_FLIP_NONE);
+      }//end of for loop
+    }//end of for loop
 
-            if(makeRope == 0)
-            {
-		xPos+=125;
-
-                ropes[y][x].setMidPointX(xPos);
-	        ropes[y][x].setMidPointY(yPos);
-	        angle = rand() % 2;
-                ropes[y][x].setAngle(angle);
-                ropes[y][x].setTexture("/images/Rope.png", renderer);
-            }
-	}
-		yPos += 125;
-		xPos = 0;
-    }
+    SDL_RenderFillRect(renderer, &player);
 
 
-    //running game
-    bool running = true;
-    SDL_Event event;
+    //outputs the renderer
+    SDL_RenderPresent(renderer);
 
-    //player rect for testing.
-    SDL_Renderer* rectRender = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
-    SDL_Rect player = {0, 0, 30, 30};
+    //restricts to 60fps
+    SDL_Delay(1000/60);
 
+  }//end of while loop
 
-    while(running)
-    {
-        SDL_PollEvent(&event);
-        switch (event.type)
-        {
-            //close window
-            case SDL_QUIT:
-                running = false;
-                break;
+  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyTexture(texture);
 
-            //move the player
-            //unsure how to do diagonal
-            case SDL_KEYDOWN:
-                switch(event.key.keysym.sym)
-                {
-                    case SDLK_LEFT:
-                        player.x--;
-                        break;
-                    case SDLK_RIGHT:
-                        player.x++;
-                        break;
-                    case SDLK_UP:
-                        player.y--;
-                        break;
-                    case SDLK_DOWN:
-                        player.y++;
-                        break;
-                }
-                break;
+  SDL_Quit();
 
-            default:
-                break;
-
-        }
-        //makes window background not black
-        SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
-
-        //resets the renderer
-        SDL_RenderClear(renderer);
-
-        //draws a rectangle
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
- 
-       //renders ropes to screen
-        for(int y = 0; y < total; y++)
-	{
-             for(int x = 0; x < total; x++)
-             {
-                  ropes[y][x].render(ropes[y][x].getMidPoint()->x, ropes[y][x].getMidPoint()->y, NULL, renderer, ropes[y][x].getAngle(), NULL, SDL_FLIP_NONE);
-             }
-        }
-        SDL_RenderFillRect(renderer, &player);
-
-
-        //outputs the renderer
-        SDL_RenderPresent(renderer);
-
-        //restricts to 60fps
-        SDL_Delay(1000/60);
-    }
-
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyTexture(texture);
-
-    SDL_Quit();
-
-    return 0;
+  return 0;
 }
