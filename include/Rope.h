8d30d6d1718d1bf443a083eb9a33ccb9ea6c6a14
jay pestillo <jaypestillo@gmail.com>
2017-04-23 20:04:41 -0400

    Deleted everything in the Rope.cpp class since were starting it over.

--- include/Rope.h
+++ include/Rope.h
@@ -12,52 +12,19 @@
 class Rope
 {
 	public:
-		//Temporary Declaration
-		Map ropeMap;
-
 		//Initializes variables
 		Rope();
-		//Deallocates memory
-		virtual ~Rope();
-		//Deallocates texture, when rope is jumped over
-		void free();
-		//Renders texture at given point, can also rotate
-		void render(int x, int y, SDL_Rect* rect, SDL_Renderer *renderer, double angle, SDL_Point* center, SDL_RendererFlip flip);
-		//Gets the width of the rope image
-		int getWidth();
-		//Gets the height of the rope image
-		int getHeight();
-		//Gets start point on screen
-		SDL_Point* getMidPoint();
-		//Gets angle
-		int getAngle();
-		//Gets texture
-		SDL_Texture* getTexture();
-		//Loads image at a place
-		void setTexture(std::string path, SDL_Renderer* renderer);
-		//Set point a position
-		void setMidPoint(SDL_Point point);
-		//Sets angle
-		void setAngle(int newAngle);
-
-		// Temporary functions for overhaul
-		void setRope();
-
-		void deleteRope();
+		//Set ropes function to set the ropes
+		void setRopes();
 
 	private:
-		//Image Texture
-		SDL_Texture* mTexture;
-
-		//Image dimensions
-		int mWidth;             //image is 15
-		int mHeight;            //image is 100
-
-		//Position on the map
-		SDL_Point midpoint;
-
-		//Angle
-		//Can be 0-180
-		int angle;
+		//Creates an instance of the Map for the Ropes
+		Map ropeMap;
+		//enum used to determine if the rope is horizontal or verticle.
+		enum angle
+		{
+			HORIZONTAL = 1,
+			VERTICLE = 2
+		};//end of enum angle
 };
 #endif /* ROPE_H */
